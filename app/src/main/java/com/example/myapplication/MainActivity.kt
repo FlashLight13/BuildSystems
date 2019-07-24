613adf9371311882d29f392c02d063a07dc3c482
Anton Potapov <flashlight13@fun.co>
2019-07-25 00:40:09 +0300

    Done sample

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.myapplication
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //findViewById<TextView>(R.id.text).text = "hello buck 1"
     }
 }
